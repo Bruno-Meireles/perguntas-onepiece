c56dceedfe76718a8f7f1812079bc563150c816f
Bruno Meireles <bruno.meirelessilva90@gmail.com>
2024-02-26 08:57:13 -0300

    Projetofrases motivacionais One Piece

---
+++ frase_do_dia/lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _frases = [
+    "Não importa quantas vezes você cai, o importante é sempre se levantar. - Monkey D. Luffy",
+    "Quando você está lutando pela coisa certa, não há necessidade de desistir. - Roronoa Zoro",
+    "A coragem não é a ausência do medo, mas sim a determinação de seguir em frente apesar dele. - Usopp",
+    "O verdadeiro poder vem da convicção de seguir seus sonhos, não importa o quão impossíveis eles pareçam. - Nico Robin",
+    "Não importa o quão escuro seja o presente, o amanhã sempre trará uma nova luz. - Brook",
+    "Os verdadeiros amigos são aqueles que permanecem ao seu lado, mesmo quando o mundo inteiro está contra você. - Nami",
+    "A força não está apenas na habilidade de lutar, mas na capacidade de se levantar depois de ser derrotado. - Sanji",
+    "A vida é como uma jornada no mar tempestuoso; é preciso coragem para navegar pelas ondas mais difíceis. - Tony Tony Chopper",
+    "Não deixe que as cicatrizes do passado impeçam você de alcançar um futuro brilhante. - Portgas D. Ace",
+    "O verdadeiro tesouro não está no final da jornada, mas sim nas experiências que acumulamos ao longo do caminho.-GolD.Roger",
+  ];
+
+  var _fraseGerada = "Clique abaixo para gerar uma nova frase!";
+
+  void gerarFrase() {
+    var numeroSorteado = Random().nextInt(_frases.length);
+
+    setState(() {
+      _fraseGerada = _frases[numeroSorteado];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: const Center(
+            child: Text(
+              "Frases Motivacionais One Piece",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset(
+                'images/luffy.jpg',
+                width: 200,
+              ),
+              Column(
+                children: [
+                  Text(
+                    _fraseGerada,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.red),
+                onPressed: gerarFrase,
+                child: const Text(
+                  " Nova Frase",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
