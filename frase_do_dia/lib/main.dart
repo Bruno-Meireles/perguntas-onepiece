6c6b0fd363cb26db338ab70d2a4b3b29475e73f1
Bruno Meireles <bruno.meirelessilva90@gmail.com>
2024-02-28 14:19:57 -0300

    Imagens adicionadas

--- frase_do_dia/lib/main.dart
+++ frase_do_dia/lib/main.dart
@@ -5,47 +5,45 @@ void main() {
   runApp(const MyApp());
 }
 
+class FraseMotivacional {
+  final String frase;
+  final String imagemPath;
+
+  FraseMotivacional(this.frase, this.imagemPath);
+}
+
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  final caminhoImagens = [
-    "images/luffy.jpg",
-    "images/zoro.jpg",
-    "images/usopp.jpg",
-    "images/robin.jpg",
-    "images/brook.jpg",
-    "images/nami.jpg",
-    "images/sanji.jpg",
-    "images/chopper.jpg",
-    "images/ace.jpg",
-    "images/roger.jpg",
+  final List<FraseMotivacional> frasesMotivacionais = [
+    FraseMotivacional("Não importa quantas vezes você cai, o importante é sempre se levantar.", "images/luffy.jpg"),
+    FraseMotivacional("Quando você está lutando pela coisa certa, não há necessidade de desistir.", "images/zoro.jpg"),
+    FraseMotivacional("A coragem não é a ausência do medo, mas sim a determinação de seguir em frente apesar dele.", "images/usopp.jpg"),
+    FraseMotivacional("O verdadeiro poder vem da convicção de seguir seus sonhos, não importa o quão impossíveis eles pareçam.", "images/robin.jpg"),
+    FraseMotivacional("Não importa o quão escuro seja o presente, o amanhã sempre trará uma nova luz.", "images/brook.jpg"),
+    FraseMotivacional("Os verdadeiros amigos são aqueles que permanecem ao seu lado, mesmo quando o mundo inteiro está contra você.", "images/nami.jpg"),
+    FraseMotivacional("A força não está apenas na habilidade de lutar, mas na capacidade de se levantar depois de ser derrotado.", "images/sanji.jpg"),
+    FraseMotivacional("A vida é como uma jornada no mar tempestuoso; é preciso coragem para navegar pelas ondas mais difíceis.", "images/chopper.jpg"),
+    FraseMotivacional("Não deixe que as cicatrizes do passado impeçam você de alcançar um futuro brilhante.", "images/ace.jpg"),
+    FraseMotivacional("O verdadeiro tesouro não está no final da jornada, mas sim nas experiências que acumulamos ao longo do caminho.", "images/roger.jpg"),
   ];
 
-  final _frases = [
-    "Não importa quantas vezes você cai, o importante é sempre se levantar. - Monkey D. Luffy, ",
-    "Quando você está lutando pela coisa certa, não há necessidade de desistir. - Roronoa Zoro",
-    "A coragem não é a ausência do medo, mas sim a determinação de seguir em frente apesar dele. - Usopp",
-    "O verdadeiro poder vem da convicção de seguir seus sonhos, não importa o quão impossíveis eles pareçam. - Nico Robin",
-    "Não importa o quão escuro seja o presente, o amanhã sempre trará uma nova luz. - Brook",
-    "Os verdadeiros amigos são aqueles que permanecem ao seu lado, mesmo quando o mundo inteiro está contra você. - Nami",
-    "A força não está apenas na habilidade de lutar, mas na capacidade de se levantar depois de ser derrotado. - Sanji",
-    "A vida é como uma jornada no mar tempestuoso; é preciso coragem para navegar pelas ondas mais difíceis. - Tony Tony Chopper",
-    "Não deixe que as cicatrizes do passado impeçam você de alcançar um futuro brilhante. - Portgas D. Ace",
-    "O verdadeiro tesouro não está no final da jornada, mas sim nas experiências que acumulamos ao longo do caminho.-GolD.Roger",
-  ];
+  FraseMotivacional? _fraseMotivacional;
 
-  var _fraseGerada = "Clique abaixo para gerar uma nova frase!";
+  @override
+  void initState() {
+    super.initState();
+    _gerarFraseMotivacional();
+  }
 
-  void gerarFrase() {
-    var numeroSorteado = Random().nextInt(_frases.length);
-
+  void _gerarFraseMotivacional() {
     setState(() {
-      _fraseGerada = _frases[numeroSorteado];
+      _fraseMotivacional = frasesMotivacionais[Random().nextInt(frasesMotivacionais.length)];
     });
   }
 
@@ -68,16 +66,16 @@ class _MyAppState extends State<MyApp> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Image.asset(
-                caminhoImagens[Random().nextInt(caminhoImagens.length)],
+                _fraseMotivacional!.imagemPath,
                 width: 130,
               ),
               Column(
                 children: [
                   Text(
-                    _fraseGerada,
+                    _fraseMotivacional!.frase,
                     textAlign: TextAlign.center,
                     style: const TextStyle(
-                      color: Colors.black87,
+                      color: Colors.black,
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
                     ),
@@ -85,8 +83,8 @@ class _MyAppState extends State<MyApp> {
                 ],
               ),
               ElevatedButton(
-                style: TextButton.styleFrom(backgroundColor: Colors.red),
-                onPressed: gerarFrase,
+                style: TextButton.styleFrom(backgroundColor: Colors.red ),
+                onPressed: _gerarFraseMotivacional,
                 child: const Text(
                   " Nova Frase",
                   style: TextStyle(
